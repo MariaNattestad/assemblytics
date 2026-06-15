d05b75e0ebadead36223679bb984227e5e3f7520
Maria Nattestad <maria.nattestad@gmail.com>
2026-06-15 15:30:28 +0200

    Final Cleanup: Remove legacy web app, reorganize examples, and finish D3 integration

--- public/list_plots.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $code=$_POST['code'];
-    // echo json_encode(shell_exec("ls user_data/". $code . "/*.png"));
-
-    // $pattern = "user_data/".$code."/*.png";
-    $filenames = glob("user_data/".$code."/*.png");
-    echo json_encode($filenames);
-?>
